74e0f6ef3dae0bc3bcfd9ef89d56bac2810c93e7
陈东阳 <1815366627@qq.com>
2024-11-29 21:06:31 +0800

    refactor first dev

---
+++ src/components/login/login.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    登录页面
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            redirect: '/login',
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/components/login/login.vue'),
+        },
+    ],
+})
+
+export default router
